d48383871d4ae67089e8bfa88eef3e1aafb7a61b
Jim Dowling <jdowling@sics.se>
2017-03-11 22:42:00 +0100

    fix downloadurl

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@ default.conda.version                      = '4.3.1'
 # the version of python: either '2' or '3'
 default.conda.python                       = '2'
 
-default.conda.url                          = "https://repo.continuum.io/archive/Conda#{node.conda.python}-#{node.conda.version}-Linux-x86_64.sh"
+default.conda.url                          = node.attribute?(:download_url) ? node.download_url + "/Anaconda#{node.conda.python}-#{node.conda.version}-Linux-x86_64.sh" : "https://repo.continuum.io/archive/Anaconda#{node.conda.python}-#{node.conda.version}-Linux-x86_64.sh"
 
 default.conda.user                         = node.install.user.empty? ? "anaconda" : node.install.user
 default.conda.group                        = node.install.user.empty? ? "anaconda" : node.install.user
