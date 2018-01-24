8c07309ba1e3738fdfdfb73db2a2b30e2b51709f
Fabio Buso <dev.siroibaf@gmail.com>
2018-01-24 18:01:50 +0100

    Fix conda installation

---
+++ recipes/default.rb
@@ -0,0 +1,56 @@
+# Conda needs the .conda directory, the .conda/pkgs directory and the .conda/environments.txt file
+# it is supposed to automatically create them, but it's very unpredictable when it comes to do so
+# so we create them manually here
+directory "/home/#{node['conda']['user']}/.conda/pkgs" do
+  user node['conda']['user']
+  group node['conda']['group']
+  recursive true
+end
+
+
+file "/home/#{node['conda']['user']}/.conda/environments.txt" do
+  user node['conda']['user']
+  group node['conda']['group']
+end
+
+
+bash "update_conda" do
+  user node['conda']['user']
+  group node['conda']['group']
+  environment ({'HOME' => "/home/#{node['conda']['user']}"})
+  cwd "/home/#{node['conda']['user']}"
+  code <<-EOF
+    set -e
+    #{node['conda']['base_dir']}/bin/conda update conda -y -q
+    #{node['conda']['base_dir']}/bin/conda update anaconda -y -q
+  EOF
+end
+
+#
+# Install libraries into the root environment
+#
+for lib in node["conda"]["default_libs"] do
+  bash "install_anconda_default_libs" do
+    user node['conda']['user']
+    group node['conda']['group']
+    environment ({'HOME' => "/home/#{node['conda']['user']}"})
+    cwd "/home/#{node['conda']['user']}"
+    code <<-EOF
+      #{node['conda']['base_dir']}/bin/conda install -q -y #{lib}
+    EOF
+# The guard checks if the library is installed. Be careful with library names like 'sphinx' and 'sphinx_rtd_theme' - add space so that 'sphinx' doesnt match both.
+    not_if  "#{node['conda']['base_dir']}/bin/conda list | grep \"#{lib}\"", :user => node['conda']['user']
+  end
+end
+
+
+bash "create_base" do
+  user node['conda']['user']
+  group node['conda']['group']
+  environment ({'HOME' => "/home/#{node['conda']['user']}"})
+  cwd "/home/#{node['conda']['user']}"
+  code <<-EOF
+    #{node['conda']['base_dir']}/bin/conda create -n #{node['conda']['user']}
+  EOF
+  not_if "test -d #{node['conda']['base_dir']}/envs/#{node['conda']['user']}", :user => node['conda']['user']
+end
