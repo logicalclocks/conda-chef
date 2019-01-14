3e5ad41ebfc66ec2c6e15dde0b7783338fc7f063
Fabio Buso <dev.siroibaf@gmail.com>
2019-01-14 16:57:57 +0100

    [HOPSWORKS-870] Support PIP configuration for internal PyPi servers/p… (#19)

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,6 @@ version           "0.9.0"
 
 supports 'ubuntu', '= 14.04'
 supports 'ubuntu', '= 16.04'
-
 supports 'centos', '= 7.2'
 
 depends           'magic_shell'
@@ -50,6 +49,18 @@ attribute "conda/use_defaults",
 
 attribute "conda/default_libs",
           :description => "Space separated list of libraries to be installed in Conda root environment",
+          :type => "string"
+
+attribute "pypi/index",
+          :description => "Mirror endpoint for PIP search",
+          :type => "string"
+
+attribute "pypi/index-url",
+          :description => "Mirror endpoint for PIP install and PIP actions which use PEP503 compliant API",
+          :type => "string"
+
+attribute "pypi/trusted-host",
+          :description => "Trusted host for non https pypi mirrors",
           :type => "string"
 
 attribute "conda/provided_lib_names",
