1317b92163616f0196079ce87d38ba23c1f5c2b7
Fabio Buso <buso.fabio@gmail.com>
2019-02-06 07:44:35 +0100

    Update node['install']['version'] attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,10 +12,10 @@ default["install"]["dir"]                        = "/srv/hops"
 default["install"]["current_version"]            = ""
 
 # Update target
-default["install"]["version"]                    = "0.9.0-SNAPSHOT"
+default["install"]["version"]                    = "0.10.0-SNAPSHOT"
 
 # List of released versions
-default["install"]["versions"]                     = "0.1.0,0.2.0,0.3.0,0.4.0,0.4.1,0.4.2,0.5.0,0.6.0,0.6.1,0.7.0,0.8.0,0.8.1"
+default["install"]["versions"]                     = "0.1.0,0.2.0,0.3.0,0.4.0,0.4.1,0.4.2,0.5.0,0.6.0,0.6.1,0.7.0,0.8.0,0.8.1,0.9.0"
 
 
 # These are global attributes which are inherited by all the cookbooks and therefore availabel
@@ -25,7 +25,7 @@ default["java"]["install_flavor"]                  = "openjdk"
 default['java']['set_etc_environment']             = true
 default["java"]["jdk_version"]                     = 8
 
-default["rhel"]["epel"]                            = true 
+default["rhel"]["epel"]                            = true
 
 
 default['install']['user']                         = ""
