10b1a77bd894dcf3ca5734f5f28f8e51cc37d957
Jim Dowling <jdowling@sics.se>
2019-05-17 14:13:23 +0200

    add fastai

--- attributes/default.rb
+++ attributes/default.rb
@@ -69,7 +69,7 @@ default["conda"]["default_libs"]                   = %w{ }
 #numpy hdfs3 scikit-learn matplotlib pandas
 
 # Comma separated list of preinstalled libraries users are able to uninstall
-default['conda']['provided_lib_names']           = "hops, pandas, tensorflow-serving-api, hopsfacets, numpy, matplotlib, maggy"
+default['conda']['provided_lib_names']           = "hops, pandas, tensorflow-serving-api, hopsfacets, numpy, matplotlib, maggy, fastai"
 # Comma separated list of preinstalled libraries users are not able to uninstall
 default['conda']['preinstalled_lib_names']       = "pydoop, pyspark, tensorboard, jupyter, sparkmagic, hdfscontents"
 
