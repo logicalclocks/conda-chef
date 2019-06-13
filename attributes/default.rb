368013741033facc4bb6761f612bd18339a6f2f4
Fabio Buso <buso.fabio@gmail.com>
2019-06-13 11:44:47 +0200

    [HOPSWORKS-1144] Update conda distribution binary

--- attributes/default.rb
+++ attributes/default.rb
@@ -31,7 +31,7 @@ default['install']['user']                        = ""
 
 ############################ END GLOBAL ATTRIBUTES #######################################
 
-default['conda']['version']                       = "5.2.0"
+default['conda']['version']                       = "2019.03"
 # the version of python: either '2' or '3'
 default['conda']['python']                        = "2"
 default['conda']['nvidia-ml-py']['version']       = "7.352.0"
