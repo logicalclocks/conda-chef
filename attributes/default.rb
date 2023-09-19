d7c4ff452fb1722c88f8430e9801be9758d6c933
Fabio Buso <fabio@logicalclocks.com>
2023-09-19 12:41:25 +0200

    Release version: 3.4.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -40,7 +40,7 @@ default["install"]["sudoers"]["rules"]             = "true"
 default["install"]["current_version"]             = ""
 
 # Update target
-default["install"]["version"] = "3.4.0-SNAPSHOT"
+default["install"]["version"] = "3.4.0"
 
 # List of released versions
 default["install"]["versions"] = "0.1.0,0.2.0,0.3.0,0.4.0,0.4.1,0.4.2,0.5.0,0.6.0,0.6.1,0.7.0,0.8.0,0.8.1,0.9.0,0.9.1,0.10.0,1.0.0,1.1.0,1.2.0,1.3.0,1.4.0,1.4.1,2.0.0,2.1.0,2.2.0,2.3.0,2.4.0,2.5.0,3.0.0,3.1.0,3.2.0,3.3.0"
@@ -91,7 +91,7 @@ default['conda']['beam']['version']               = "2.24.0"
 default['conda']['pydoop']['version']             = "2.0.0"
 default['conda']['nvidia-ml-py']['version']       = "7.352.0"
 
-default["conda"]["hops-util-py"]["install-mode"]  = 'git'
+default["conda"]["hops-util-py"]["install-mode"] = "pip"
 default["conda"]["hops-util-py"]["branch"]        = "master"
 default["conda"]["hops-util-py"]["repo"]          = "logicalclocks"
 default["conda"]["hops-util-py"]["minor"]         = "0"
