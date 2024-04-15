32685eda0394b805dfed7eba9c514a186d3d9f8c
Fabio Buso <fabio@hopsworks.ai>
2024-04-15 12:42:10 +0200

    Release version 3.7.2

--- attributes/default.rb
+++ attributes/default.rb
@@ -40,7 +40,7 @@ default["install"]["sudoers"]["rules"]             = "true"
 default["install"]["current_version"]             = ""
 
 # Update target
-default["install"]["version"] = "3.7.1"
+default["install"]["version"] = "3.7.2"
 
 # List of released versions
 default["install"]["versions"] = "0.1.0,0.2.0,0.3.0,0.4.0,0.4.1,0.4.2,0.5.0,0.6.0,0.6.1,0.7.0,0.8.0,0.8.1,0.9.0,0.9.1,0.10.0,1.0.0,1.1.0,1.2.0,1.3.0,1.4.0,1.4.1,2.0.0,2.1.0,2.2.0,2.3.0,2.4.0,2.5.0,3.0.0,3.1.0,3.2.0,3.3.0,3.4.0,3.5.0"
